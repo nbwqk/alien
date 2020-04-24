534093525a6238468f1a0cadc8a22722a92e50be
Administrator <52603245@qq.com>
2020-04-24 14:39:53 +0800

    射击外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,7 +26,8 @@ def run_game():
         gf.check_events(ai_settings,screen,ship,bullets)
 
         ship.update()
-        gf.update_bullets(bullets)
+        gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+        gf.update_aliens(ai_settings,ship,aliens)
         # 每次循环时都重绘屏幕
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
--- game_functions.py
+++ game_functions.py
@@ -50,15 +50,49 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
-def update_bullets(bullets):
+def check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets):
+    # 检查是否有子弹击中了外星人
+    # 如果是这样，就删除相应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # 删除现有的子弹并新建一群外星人
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+
+def update_bullets(ai_settings,screen,ship,aliens,bullets):
     """更新子弹的位置，并删除已消失的子弹"""
     # 更新子弹的位置
     bullets.update()
-
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
     # 删除已消失的子弹
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+
+
+def check_fleet_edges(ai_settings,aliens):
+    """有外星人到达边缘时采取相应的措施"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings,aliens)
+            break
+
+def change_fleet_direction(ai_settings,aliens):
+    """将整群外星人下移，并改变它们的方向"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+def update_aliens(ai_settings,ship,aliens):
+    # 检查是否有外星人位于屏幕边缘，并更新整群外星人的位置
+    check_fleet_edges(ai_settings,aliens)
+    aliens.update()
+
+    # 检测外星人和飞船之间的碰撞
+    if pygame.sprite.spritecollideany(ship,aliens):
+        print("ship hit!!!")
 
 def get_number_aliens_x(ai_settings,alien_width):
     """计算每行可容纳多少个外星人"""
@@ -92,4 +126,6 @@ def create_fleet(ai_settings,screen,ship,aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
              create_alien(ai_settings,screen,aliens,alien_number,row_number)
+
+
 
