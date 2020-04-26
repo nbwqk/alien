223ed740ea45df42e4bfd9524a76746e4bba7361
Administrator <52603245@qq.com>
2020-04-26 12:21:35 +0800

    设置开始按钮

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
 
 def run_game():
     # 初始化游戏，并创建一个屏幕对象
@@ -11,6 +12,9 @@ def run_game():
     ai_settings=Settings()
     screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建Play按钮
+    play_button=Button(ai_settings,screen,"play")
 
     # 创建一个用于存储游戏统计信息的实例
     stats=GameStats(ai_settings)
@@ -27,12 +31,13 @@ def run_game():
     while True:
 
         # 监视键盘和鼠标事件
-        gf.check_events(ai_settings,screen,ship,bullets)
+        gf.check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets)
 
-        ship.update()
-        gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
-        gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
         # 每次循环时都重绘屏幕
-        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -27,7 +27,7 @@ def check_up_events(event,ship):
     if event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings,screen,ship,bullets):
+def check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -39,7 +39,31 @@ def check_events(ai_settings,screen,ship,bullets):
         elif event.type==pygame.KEYUP:
             check_up_events(event,ship)
 
-def update_screen(ai_settings,screen,ship,aliens,bullets):
+        elif event.type==pygame.MOUSEBUTTONDOWN:
+            mouse_x,mouse_y=pygame.mouse.get_pos()
+            check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
+bullets, mouse_x, mouse_y)
+
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
+bullets, mouse_x, mouse_y):
+    """在玩家单击Play按钮时开始新游戏"""
+    button_clicked=play_button.rect.collidepoint(mouse_x,mouse_y)
+    if button_clicked and not stats.game_active :
+        # 隐藏光标
+        pygame.mouse.set_visible(False)
+        # 重置游戏统计信息
+        stats.reset_stats()
+        stats.game_active=True
+
+        # 清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty()
+
+        # 创建一群新的外星人，并让飞船居中
+        create_fleet(ai_settings,screen,ship,aliens)
+        ship.center_ship()
+
+def update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -47,6 +71,10 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    # 如果游戏处于非活动状态，就绘制Play按钮
+    if not stats.game_active:
+        play_button.draw_button()
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
@@ -132,19 +160,23 @@ def create_fleet(ai_settings,screen,ship,aliens):
 
 def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
     """响应被外星人撞到的飞船"""
-    # 将ships_left减1
-    stats.ships_left -= 1
+    if stats.ships_left>0:
+        # 将ships_left减1
+        stats.ships_left -= 1
 
-    # 清空外星人列表和子弹列表
-    aliens.empty()
-    bullets.empty()
+        # 清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty()
 
-    # 创建一群新的外星人，并将飞船放到屏幕底端中央
-    create_fleet(ai_settings,screen,ship,aliens)
-    ship.center_ship()
+        # 创建一群新的外星人，并将飞船放到屏幕底端中央
+        create_fleet(ai_settings,screen,ship,aliens)
+        ship.center_ship()
 
-    # 暂停
-    sleep(0.5)
+        # 暂停
+        sleep(0.5)
+    else:
+        stats.game_active=False
+        pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
     """检查是否有外星人到达了屏幕底端"""
